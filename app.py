33bfee3fdab1c7b866192cfdeb84d074a0095c48
Ben Hakimi <b.j.hakimi@gmail.com>
2021-06-02 10:47:37 -0500

    update app.py naming

--- app.py
+++ app.py
@@ -1,56 +1,209 @@
-import traceback
-import logging.config
+import string
+import random
 from flask import Flask
-from flask import render_template, request, redirect, url_for
+import traceback
+from flask import request, render_template, redirect, url_for
+import pickle
+from numpy import testing
+import pandas as pd
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score, confusion_matrix, accuracy_score
+from src.createDB import inputGames
 
-# Initialize the Flask application
 app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
 
-# Configure flask app from flask_config.py
-app.config.from_pyfile('config/flaskconfig.py')
+@app.route('/', methods = ['GET', 'POST'])
+def home_page():
+    if request.method == 'GET':
+        return render_template('index.html')
 
-# Define LOGGING_CONFIG in flask_config.py - path to config file for setting
-# up the logger (e.g. config/logging/local.conf)
-logging.config.fileConfig(app.config["LOGGING_CONFIG"])
-logger = logging.getLogger(app.config["APP_NAME"])
-logger.debug('Web app log')
+    if request.method == 'POST':
+        homeTeam = request.form['homeT']
+        awayTeam = request.form['awayT']
+        spread = request.form['pickSpread']
+        url_for_post = url_for('addThem', homeTeam=homeTeam, awayTeam=awayTeam, spread=spread)
+        return redirect(url_for_post)
 
-# Initialize the database session
-from src.createDB import Games, inputGames
-input_games = inputGames(app)
+@app.route('/prediction/<homeTeam>/<awayTeam>/<spread>', methods = ['GET', 'POST'])
+def addThem(homeTeam, awayTeam, spread):
+    if request.method == 'GET': 
+        with open('models/model.pkl', 'rb') as f:
+            rf = pickle.load(f)
 
-@app.route('/')
-def index():
-    """Main view that lists songs in the database.
-    Create view into index page that uses data queried from Track database and
-    inserts it into the msiapp/templates/index.html template.
-    Returns: rendered html template
-    """
+        colnames = ['homeSpread','ArizonaV','AtlantaV','BaltimoreV','BuffaloV','CarolinaV','ChicagoV','CincinnatiV','ClevelandV',
+                            'DallasV','DenverV','DetroitV','GreenBayV','HoustonV','IndianapolisV','JacksonvilleV','KansasCityV',
+                            'LAChargersV','LARamsV','LVRaidersV','MiamiV','MinnesotaV','NYGiantsV','NYJetsV','NewEnglandV',
+                            'NewOrleansV','PhiladelphiaV','PittsburghV','SanFranciscoV','SeattleV','TampaBayV','TennesseeV',
+                            'WashingtonV','Arizona','Atlanta','Baltimore','Buffalo','Carolina','Chicago','Cincinnati',
+                            'Cleveland','Dallas','Denver','Detroit','GreenBay','Houston','Indianapolis','Jacksonville',
+                            'KansasCity','LAChargers','LARams','LVRaiders','Miami','Minnesota','NYGiants','NYJets',
+                            'NewEngland','NewOrleans','Philadelphia','Pittsburgh','SanFrancisco','Seattle','TampaBay',
+                            'Tennessee','Washington']
+        inputs = [0,    0,    0,    0,    0,    0,    0,    0,    0,
+           0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+           0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+           0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+           0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+           0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+           0]
 
-    try:
-        tracks = input_games.session.query(Games).limit(app.config["MAX_ROWS_SHOW"]).all()
-        logger.debug("Index page accessed")
-        return render_template('index.html', tracks=tracks)
-    except:
-        traceback.print_exc()
-        logger.warning("Not able to display tracks, error page returned")
-        return render_template('error.html')
+
+        testdata = pd.DataFrame([inputs],columns = colnames)
+        spreadNum = float(spread)
+        testdata['homeSpread']=spreadNum
+        if homeTeam == "Arizona":
+            testdata['Arizona']=1
+        elif homeTeam == "Atlanta":
+            testdata['Atlanta']=1
+        elif homeTeam == "Baltimore":
+            testdata['Baltimore']=1
+        elif homeTeam == "Buffalo":
+            testdata['Buffalo']=1
+        elif homeTeam == "Carolina":
+            testdata['Carolina']=1
+        elif homeTeam == "Chicago":
+            testdata['Chicago']=1
+        elif homeTeam == "Cincinnati":
+            testdata['Cincinnati']=1
+        elif homeTeam == "Cleveland":
+            testdata['Cleveland']=1
+        elif homeTeam == "Dallas":
+            testdata['Dallas']=1
+        elif homeTeam == "Denver":
+            testdata['Denver']=1
+        elif homeTeam == "Detroit":
+            testdata['Detroit']=1
+        elif homeTeam == "GreenBay":
+            testdata['GreenBay']=1
+        elif homeTeam == "Houston":
+            testdata['Houston']=1
+        elif homeTeam == "Indianapolis":
+            testdata['Indianapolis']=1
+        elif homeTeam == "Jacksonville":
+            testdata['Jacksonville']=1
+        elif homeTeam == "KansasCity":
+            testdata['KansasCity']=1
+        elif homeTeam == "LasVegas":
+            testdata['LVRaiders']=1
+        elif homeTeam == "LosAngelesChargers":
+            testdata['LAChargers']=1
+        elif homeTeam == "LosAngelesRams":
+            testdata['LARams']=1
+        elif homeTeam == "Miami":
+            testdata['Miami']=1
+        elif homeTeam == "Minnesota":
+            testdata['Minnesota']=1
+        elif homeTeam == "NewEngland":
+            testdata['NewEngland']=1
+        elif homeTeam == "NewOrleans":
+            testdata['NewOrleans']=1
+        elif homeTeam == "NewYorkGiants":
+            testdata['NYGiants']=1
+        elif homeTeam == "NewYorkJets":
+            testdata['NYJets']=1
+        elif homeTeam == "Philadelphia":
+            testdata['Philadelphia']=1
+        elif homeTeam == "Pittsburgh":
+            testdata['Pittsburgh']=1
+        elif homeTeam == "SanFrancisco":
+            testdata['SanFrancisco']=1
+        elif homeTeam == "Seattle":
+            testdata['Seattle']=1
+        elif homeTeam == "TampaBay":
+            testdata['TampaBay']=1
+        elif homeTeam == "Tennessee":
+            testdata['Tennessee']=1
+        elif homeTeam == "Washington":
+            testdata['Washington']=1
+
+        if awayTeam == "Arizona":
+            testdata['ArizonaV']=1
+        elif awayTeam == "Atlanta":
+            testdata['AtlantaV']=1
+        elif awayTeam == "Baltimore":
+            testdata['BaltimoreV']=1
+        elif awayTeam == "Buffalo":
+            testdata['BuffaloV']=1
+        elif awayTeam == "Carolina":
+            testdata['CarolinaV']=1
+        elif awayTeam == "Chicago":
+            testdata['ChicagoV']=1
+        elif awayTeam == "Cincinnati":
+            testdata['CincinnatiV']=1
+        elif awayTeam == "Cleveland":
+            testdata['ClevelandV']=1
+        elif awayTeam == "Dallas":
+            testdata['DallasV']=1
+        elif awayTeam == "Denver":
+            testdata['DenverV']=1
+        elif awayTeam == "Detroit":
+            testdata['DetroitV']=1
+        elif awayTeam == "GreenBay":
+            testdata['GreenBayV']=1
+        elif awayTeam == "Houston":
+            testdata['HoustonV']=1
+        elif awayTeam == "Indianapolis":
+            testdata['IndianapolisV']=1
+        elif awayTeam == "Jacksonville":
+            testdata['JacksonvilleV']=1
+        elif awayTeam == "KansasCity":
+            testdata['KansasCityV']=1
+        elif awayTeam == "LasVegas":
+            testdata['LVRaidersV']=1
+        elif awayTeam == "LosAngelesChargers":
+            testdata['LAChargersV']=1
+        elif awayTeam == "LosAngelesRams":
+            testdata['LARamsV']=1
+        elif awayTeam == "Miami":
+            testdata['MiamiV']=1
+        elif awayTeam == "Minnesota":
+            testdata['MinnesotaV']=1
+        elif awayTeam == "NewEngland":
+            testdata['NewEnglandV']=1
+        elif awayTeam == "NewOrleans":
+            testdata['NewOrleansV']=1
+        elif awayTeam == "NewYorkGiants":
+            testdata['NYGiantsV']=1
+        elif awayTeam == "NewYorkJets":
+            testdata['NYJetsV']=1
+        elif awayTeam == "Philadelphia":
+            testdata['PhiladelphiaV']=1
+        elif awayTeam == "Pittsburgh":
+            testdata['PittsburghV']=1
+        elif awayTeam == "SanFrancisco":
+            testdata['SanFranciscoV']=1
+        elif awayTeam == "Seattle":
+            testdata['SeattleV']=1
+        elif awayTeam == "TampaBay":
+            testdata['TampaBayV']=1
+        elif awayTeam == "Tennessee":
+            testdata['TennesseeV']=1
+        elif awayTeam == "Washington":
+            testdata['WashingtonV']=1
+
+        x = rf.predict(testdata)[0]
+
+        def id_generator(size, chars=string.ascii_uppercase + string.digits):
+            return ''.join(random.choice(chars) for _ in range(size))
 
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    """View that process a POST with new song input
-    :return: redirect to index page
-    """
+        randKey = id_generator(20)
 
-    try:
-        input_games.add_game(artist=request.form['artist'], album=request.form['album'], title=request.form['title'])
-        logger.info("New song added: %s by %s", request.form['title'], request.form['artist'])
-        return redirect(url_for('index'))
-    except:
-        logger.warning("Not able to display tracks, error page returned")
-        return render_template('error.html')
+        ig = inputGames(engine_string='sqlite:///data/msia423_db.db')
+        #ig.add_game(game_id=randKey, home_team=homeTeam, away_team=awayTeam, spread=spreadNum)
+
+        if homeTeam == "home":
+            return render_template('info.html')
+        elif homeTeam == awayTeam:
+            return render_template('sameTeam.html')
+        elif int(x) == 0:
+            ig.add_game(game_id=randKey, home_team=homeTeam, away_team=awayTeam, spread=spreadNum, home_cover=int(x))
+            return render_template('awayCover.html', awayTeam=awayTeam)
+        elif int(x) == 1:
+            ig.add_game(game_id=randKey, home_team=homeTeam, away_team=awayTeam, spread=spreadNum, home_cover=int(x))
+            return render_template('homeCover.html', homeTeam=homeTeam)
 
 
 if __name__ == '__main__':
-    app.run(debug=app.config["DEBUG"], port=app.config["PORT"], host=app.config["HOST"])
+    app.run()
